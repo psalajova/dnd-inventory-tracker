a6907e54726fca0066ef0a507b55824fbbf5c1a1
Patricia Salajova <salajova.p@gmail.com>
2026-07-18 17:37:10 +0200

    Setup github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Relative paths so GitHub Pages works at any repo URL
-  base: './',
+  base: '/dnd-inventory-tracker/',
 });
